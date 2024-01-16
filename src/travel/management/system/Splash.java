702795341978a0d1937a7369ea1bfa5adc700312
Grant Nilson <gnilson314@gmail.com>
2024-01-16 15:11:13 -0800

    added splash class basics

--- src/travel/management/system/Splash.java
+++ src/travel/management/system/Splash.java
@@ -1,11 +1,13 @@
-
 package travel.management.system;
+import javax.swing.*; 
 
-
-public class Splash {
+public class Splash extends JFrame {
+    Splash() {
+        setSize(1200, 600); 
+    }
     
    public static void main (String [] args) {
+       new Splash(); 
        
    }
-    
 }
