012a0177c67c8fa963f5195424b9b6e1915fc78e
Grant Nilson <gnilson314@gmail.com>
2024-01-16 17:01:31 -0800

    instantiated spalsh with runnable

--- src/travel/management/system/Splash.java
+++ src/travel/management/system/Splash.java
@@ -2,10 +2,12 @@ package travel.management.system;
 import javax.swing.*; 
 import java.awt.*; 
 
-public class Splash extends JFrame {
+public class Splash extends JFrame implements Runnable {
+    Thread thread; 
+    
     Splash() {
-        setSize(1200, 600); 
-        setLocation(200,100); 
+//        setSize(1200, 600); 
+//        setLocation(200,100); 
         
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/Splash.jpg")); 
         Image i2 = i1.getImage().getScaledInstance(1200, 600, Image.SCALE_DEFAULT);
@@ -14,11 +16,30 @@ public class Splash extends JFrame {
         add(image); 
         
         
-        setVisible(true); 
+        setVisible(true);
+        thread = new Thread(this);
+        thread.start();
     }
     
+    public void run (){
+        try{
+            Thread.sleep(7000);
+            setVisible(false); 
+        } catch (Exception e) {
+            
+        }
+    }; 
+    
    public static void main (String [] args) {
-       new Splash(); 
+       Splash frame = new Splash(); 
+       int x = 1; 
+       for (int i = 1; i<=500;x+=7, i+=6){
+           frame.setLocation(750-(x+i/2),400-(i/2));
+           frame.setSize(x+i, i);
+           try{
+               Thread.sleep(10);
+           } catch (Exception e){}
+       }
        
    }
 }
