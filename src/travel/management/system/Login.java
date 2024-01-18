324de2f879debd939f53467c1abb3abdcd03ce84
Grant Nilson <gnilson314@gmail.com>
2024-01-17 22:14:05 -0800

    init login class

---
+++ src/travel/management/system/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package travel.management.system;
+
+/**
+ *
+ * @author grantnilson
+ */
+public class Login {
+    Login(){
+    }
+    
+    public static void main(String args[]){
+        Login l = new Login(); 
+    
+    }
+    
+}
