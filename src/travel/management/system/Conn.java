f1475f31e586f16e7211db0452c70fdf3dc211fd
Grant Nilson <gnilson314@gmail.com>
2024-01-26 16:23:41 -0800

    incremental while i connect the mysql to mac lol

--- src/travel/management/system/Conn.java
+++ src/travel/management/system/Conn.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package travel.management.system;
 
-/**
- *
- * @author grantnilson
- */
+package travel.management.system;
+import java.sql.*; 
+
 public class Conn {
+    Conn (){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+            c = DriverManager.getConnection("jdbc:mysql://travelmanagementsystem/", "root", "grantnilson"); 
+            s = c.createStatement(); 
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
     
 }
